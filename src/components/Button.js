91d97fe33dbabe987d4de5355e2c1d1c8f512049
Thomas O'Leary <t.oleary@me.com>
2021-08-31 21:28:10 -0700

    refactor button, initial DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,15 @@
 import React from "react";
+import classNames from 'classnames'
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   let buttonClass = "button";
- 
-   if (props.confirm) {
-     buttonClass += " button--confirm";
-   }
- 
-   if (props.danger) {
-     buttonClass += " button--danger";
-   }
- 
+   
+   const buttonClasses = classNames('button', {'button--confirm': props.confirm, 'button--danger': props.danger});
+
    return (
      <button
-       className={buttonClass}
+       className={buttonClasses}
        onClick={props.onClick}
        disabled={props.disabled}
      >
