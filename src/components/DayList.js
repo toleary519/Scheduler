2450cac3e92c9afdc656c5e1ea8930290c2ea556
Thomas O'Leary <t.oleary@me.com>
2021-09-01 13:01:00 -0700

    iterate over DayList to show availability

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -1,8 +1,37 @@
 
+import React from "react";
+import DayListItem from "components/DayListItem";
+
+
 export default function DayList(props) {
   
-  return (
-    <ul></ul> 
-  )
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+  ];
 
-};
+  return (
+    <ul> {
+      days.map((day) => (
+        <DayListItem 
+        name={day.name} 
+        spots={day.spots} 
+        selected={day.name === props.day}
+        setDay={props.setDay}  />
+        )  
+      )}
+    </ul>
+  )};
